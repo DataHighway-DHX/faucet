904c102366bc73b6bf8663dcee11683c3c6f6112
Luke Schoen <ltfschoen@gmail.com>
2020-06-27 15:24:38 +0200

    connect to ropsten testnet. show latest block and Ethereum address

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Setup
+# Development
 
 ```
 nvm use;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+require('dotenv').config()
+const debug = require('debug')('app')
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require('web3');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+let infuraHttpProvider = "https://ropsten.infura.io/v3/" + process.env.INFURA_API_PROJECT_ID;
+let provider = new HDWalletProvider(process.env.MNENOMIC, infuraHttpProvider);
+const web3 = new Web3(provider);
+// Show Ethereum address associated with mnemonic
+web3.eth.getCoinbase()
+  .then(debug);
+
+// Show latest block
+(async () => {
+  const block = await web3.eth.getBlock("latest");
+  debug('Current block: ', block.timestamp);
+  process.exit(0);
+})();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
