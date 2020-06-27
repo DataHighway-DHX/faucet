1dbd29c235a12b44c75a1cb62fc9c0bb3e2d0e56
Luke Schoen <ltfschoen@gmail.com>
2020-06-27 14:08:36 +0200

    wip

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # Setup
 
 ```
+yarn global add nodemon;
 yarn dev;
 ```
 
 http://localhost:3000
+
+# Deploy (Heroku)
+
+```
+heroku login
+heroku apps:create faucet-mining
+git push -f heroku master
+heroku local web
+heroku ps:scale web=1:free
+heroku ps
+heroku open
+heroku logs --tail
+heroku restart
+```
+
+# References
+
+https://www.freecodecamp.org/news/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0/
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,5 +18,15 @@ app.post('/api/world', (req, res) => {
     `Received POST request: ${req.body.post}`,
   );
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
