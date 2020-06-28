4e7fbf005805cacb0f3a9989da74ebe81e845fc3
Luke Schoen <ltfschoen@gmail.com>
2020-06-28 04:53:48 +0200

    setup faucet of MXC ERC-20 tokens

--- helpers/contract.js
+++ helpers/contract.js
@@ -1,40 +1,37 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const TruffleContract = require("@truffle/contract");
 const BN = require('bn.js');
 const debug = require('debug')('app');
 
 const DApp = {
   contractAddressMXC: null,
   contractInstanceMXC: null,
-  contractJSONMXC: null,
+  contractJSONMXCABI: null,
   coinbase: null,
   init: async () => {
     DApp.infuraHttpProvider = 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_PROJECT_ID;
     DApp.provider = new HDWalletProvider(process.env.MNENOMIC, DApp.infuraHttpProvider);
     DApp.web3 = new Web3(DApp.provider);
-    DApp.contractAddressMXC = process.env.CONTRACT_ADDRESS_MXC_TESTNET;
-    DApp.contractJSONMXC = JSON.parse(fs.readFileSync('./assets/data/MXCToken.json').toString());
-    DApp.contractInstanceMXC = TruffleContract(DApp.contractJSONMXC);
-    DApp.contractInstanceMXC.setProvider(DApp.provider);
-    // Use .deployed() instead when using development chain
-    DApp.contractInstanceMXC.at(DApp.contractAddressMXC);
     DApp.coinbase = await DApp.web3.eth.getCoinbase();
+    DApp.initMxc();
     DApp.getBlock();
+  },
+  initMxc: async () => {
+    DApp.contractAddressMXC = process.env.CONTRACT_ADDRESS_MXC_TESTNET;
+    DApp.contractJSONMXCABI = JSON.parse(fs.readFileSync('./assets/data/MXCToken.json').toString());
+    DApp.contractInstanceMXC = await new DApp.web3.eth.Contract(DApp.contractJSONMXCABI, DApp.contractAddressMXC);
+    DApp.contractInstanceMXC.setProvider(DApp.provider);
   },
   infuraHttpProvider: null,
   getBlock: async () => {
-    // FIXME - encounter same error as here https://github.com/MetaMask/web3-provider-engine/issues/313
-    // so web3.eth needs a fix similar to the one MetaMask made (i.e. `getBlockByNumberWithRetry`)
-    // consider switching to using https://github.com/MetaMask/web3-provider-engine
-    // const block = await DApp.web3.eth.getBlock("latest");
-    // debug('Current block: ', block.timestamp);
+    const block = await DApp.web3.eth.getBlock("latest");
+    debug('Current block: ', block.timestamp);
   },
   provider: null,
   sendTransactionEth: async (to) => {
     debug('Recipient: ', to);
-    debug('Sending Funds. Please wait...');
+    debug('Sending Ropsten Eth. Please wait...');
     // Show Ethereum address associated with mnemonic
     const amount = new BN(1, 10);
     // https://web3js.readthedocs.io/en/v1.2.6/web3-utils.html#towei
@@ -44,11 +41,11 @@ const DApp = {
       value: DApp.web3.utils.toWei(amount, 'wei'),
       gas: 21000
     })
-      .then((receipt) => {
-        debug('Transaction receipt', receipt);
+    .then((receipt) => {
+      debug('Transaction receipt', receipt);
 
-        return receipt.transactionHash;
-      });
+      return receipt.transactionHash;
+    });
 
     return {
       transactionHashUrl: `https://ropsten.etherscan.io/tx/${transactionHash}`
@@ -56,17 +53,25 @@ const DApp = {
   },
   sendTransactionMxc: async (to) => {
     debug('Recipient: ', to);
-    debug('Sending Funds. Please wait...');
+    debug('Sending MXC ERC-20 tokens. Please wait...');
     // Show Ethereum address associated with mnemonic
     const amount = new BN(1, 10);
     // https://web3js.readthedocs.io/en/v1.2.6/web3-utils.html#towei
-    // const approved = await DApp.contractInstanceMXC.methods.approve(DApp.coinbase,  amount);
-    DApp.contractInstanceMXC.at(DApp.contractAddressMXC);
-    const transactionHash = await DApp.contractInstanceMXC.methods.transfer(
-      to.toLowerCase(),
-      amount
-    )
-    debug('transactionHash', transactionHash);
+    const balance = await DApp.contractInstanceMXC.methods.balanceOf(DApp.coinbase).call();
+    debug('Faucet balance: ', balance);
+
+    const transactionHash = await DApp.contractInstanceMXC.methods.transfer(to.toLowerCase(), amount)
+      .send({
+        from: DApp.coinbase,
+        // https://ethgasstation.info/
+        gas: 200000,
+        gasPrice: 30000000000
+      })
+      .then((receipt) => {
+        debug('Transaction receipt', receipt);
+
+        return receipt.transactionHash;
+      });
 
     return {
       transactionHashUrl: `https://ropsten.etherscan.io/tx/${transactionHash}`
