23ed7bde0589a3c5fc0fc398775020ee89b7a7c6
Luke Schoen <ltfschoen@gmail.com>
2020-06-28 03:33:32 +0200

    wip: causes issues #1 and #2

--- helpers/contract.js
+++ helpers/contract.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const TruffleContract = require("@truffle/contract");
 const BN = require('bn.js');
 const debug = require('debug')('app');
 
 const DApp = {
+  contractAddressMXC: null,
   contractInstanceMXC: null,
   contractJSONMXC: null,
   coinbase: null,
@@ -12,17 +14,22 @@ const DApp = {
     DApp.infuraHttpProvider = 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_PROJECT_ID;
     DApp.provider = new HDWalletProvider(process.env.MNENOMIC, DApp.infuraHttpProvider);
     DApp.web3 = new Web3(DApp.provider);
+    DApp.contractAddressMXC = process.env.CONTRACT_ADDRESS_MXC_TESTNET;
     DApp.contractJSONMXC = JSON.parse(fs.readFileSync('./assets/data/MXCToken.json').toString());
-    DApp.contractInstanceMXC = new DApp.web3.eth.Contract(DApp.contractJSONMXC, process.env.CONTRACT_ADDRESS_MXC_TESTNET,
-      { gasPrice: '45000', from: process.env.ETHEREUM_ADDRESS });
+    DApp.contractInstanceMXC = TruffleContract(DApp.contractJSONMXC);
     DApp.contractInstanceMXC.setProvider(DApp.provider);
+    // Use .deployed() instead when using development chain
+    DApp.contractInstanceMXC.at(DApp.contractAddressMXC);
     DApp.coinbase = await DApp.web3.eth.getCoinbase();
     DApp.getBlock();
   },
   infuraHttpProvider: null,
   getBlock: async () => {
-    const block = await DApp.web3.eth.getBlock("latest");
-    debug('Current block: ', block.timestamp);
+    // FIXME - encounter same error as here https://github.com/MetaMask/web3-provider-engine/issues/313
+    // so web3.eth needs a fix similar to the one MetaMask made (i.e. `getBlockByNumberWithRetry`)
+    // consider switching to using https://github.com/MetaMask/web3-provider-engine
+    // const block = await DApp.web3.eth.getBlock("latest");
+    // debug('Current block: ', block.timestamp);
   },
   provider: null,
   sendTransactionEth: async (to) => {
@@ -42,6 +49,24 @@ const DApp = {
 
         return receipt.transactionHash;
       });
+
+    return {
+      transactionHashUrl: `https://ropsten.etherscan.io/tx/${transactionHash}`
+    };
+  },
+  sendTransactionMxc: async (to) => {
+    debug('Recipient: ', to);
+    debug('Sending Funds. Please wait...');
+    // Show Ethereum address associated with mnemonic
+    const amount = new BN(1, 10);
+    // https://web3js.readthedocs.io/en/v1.2.6/web3-utils.html#towei
+    // const approved = await DApp.contractInstanceMXC.methods.approve(DApp.coinbase,  amount);
+    DApp.contractInstanceMXC.at(DApp.contractAddressMXC);
+    const transactionHash = await DApp.contractInstanceMXC.methods.transfer(
+      to.toLowerCase(),
+      amount
+    )
+    debug('transactionHash', transactionHash);
 
     return {
       transactionHashUrl: `https://ropsten.etherscan.io/tx/${transactionHash}`
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const { DApp: {
-  web3, provider, contractInstanceMXC, sendTransactionEth
+  web3, provider, contractInstanceMXC, sendTransactionEth, sendTransactionMxc
 } } = require('./helpers/contract.js');
 // debug('provider', provider);
 // debug('contract', contractInstanceMXC);
@@ -25,6 +25,26 @@ app.get('/api/faucet/eth/ropsten', async (req, res, next) => {
     const { transactionHashUrl } = await sendTransactionEth(to);
     res.send({
       result: 'Ropsten Ether sent',
+      tx: transactionHashUrl
+    });
+  } catch (error) {
+    debug(error);
+    // Handle error by Express or pass to custom error handler
+    // return next(error); // Crashes
+    return;
+  }
+});
+
+/**
+ * Example: http://localhost:5000/api/faucet/mxc/ropsten?address=0x1f7ace08af5c49a5d69fbb98fb9339a729b27161
+ */
+app.get('/api/faucet/mxc/ropsten', async (req, res, next) => {
+  // Handle error in async function
+  try {
+    const to = req.query.address;
+    const { transactionHashUrl } = await sendTransactionMxc(to);
+    res.send({
+      result: 'Ropsten MXC ERC-20 sent',
       tx: transactionHashUrl
     });
   } catch (error) {
